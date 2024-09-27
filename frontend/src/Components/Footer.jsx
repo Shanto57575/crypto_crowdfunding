3d6ddca98599e13b19f1b42b1af0f90dd8427d7d
Akil Tajwar <akiltajwar@gmail.com>
2024-09-28 02:04:47 +0600

    navbar, footer, homepage

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div className="bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#891fa7] via-[#332583] to-[#13103d]">
+            <div className="flex justify-between py-10 w-3/4 mx-auto">
+                <div className="flex flex-col">
+                    <h6 className="footer-title">Company</h6>
+                    <Link className="link link-hover">About us</Link>
+                    <Link className="link link-hover">Contact</Link>
+                    <Link className="link link-hover">Jobs</Link>
+                    <Link className="link link-hover">Press kit</Link>
+                </div>
+                <div className="flex flex-col">
+                    <h6 className="footer-title">Legal</h6>
+                    <Link className="link link-hover">Terms of use</Link>
+                    <Link className="link link-hover">Privacy policy</Link>
+                    <Link className="link link-hover">Cookie policy</Link>
+                </div>
+                <div className="flex flex-col">
+                    <h6 className="footer-title">Services</h6>
+                    <Link className="link link-hover">Branding</Link>
+                    <Link className="link link-hover">Design</Link>
+                    <Link className="link link-hover">Marketing</Link>
+                    <Link className="link link-hover">Advertisement</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/Layout/Main.jsx
@@ -0,0 +1,25 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
+function Main() {
+    const location = useLocation();
+
+    const shouldRenderHeaderFooter = ![
+        "/login",
+        "/dashboard",
+        "/signup",
+    ].includes(location.pathname);
+
+    return (
+        <div className="">
+            {shouldRenderHeaderFooter && <div className=""><Navbar /></div>}
+            <div className="min-h-[79vh]">
+                <Outlet />
+            </div>
+            {shouldRenderHeaderFooter && <Footer />}
+        </div>
+    );
+}
+
+export default Main;
