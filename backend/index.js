a5d4a75cdf541d76f04a85023644013e5e98dc34
Shanto57575 <shanto57575@gmail.com>
2024-12-11 19:48:52 +0600

    done

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ connectDB();
 
 app.use(cors({
     origin: (origin, callback) => {
-        const allowedOrigins = ['https://fundchain7.netlify.app', 'http://localhost:5173'];
+        const allowedOrigins = [
+            'https://fundchain7.netlify.app',
+            'https://fundchain3.vercel.app',
+            'http://localhost:5173'];
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true);
         } else {
@@ -31,7 +34,6 @@ app.use(cors({
         }
     }
 }));
-
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
